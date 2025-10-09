5591045c1ded0b5de52738063d6ed2153daeee2c
Mateus Ferreira <mtsf26@gmail.com>
2025-10-09 11:49:54 -0300

    chore(frontend): auto-abrir navegador (dev/preview) e ajustar build outDir; incluir redirects para Netlify

--- Anatomy Game Design System/vite.config.ts
+++ Anatomy Game Design System/vite.config.ts
@@ -1,4 +1,4 @@
-
+﻿
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
@@ -55,6 +55,10 @@
     },
     server: {
       port: 3000,
+      open: true,
+        },
+    preview: {
+      port: 4173,
       open: true,
     },
   });
